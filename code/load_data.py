09d7fc8ccfeb0993be0df5fce74fc592926acb68
Will Roddy <wtroddy@udel.edu>
2020-06-14 16:21:11 -0400

    updated load data script, split sqlite db creation into its own file

--- code/load_data.py
+++ code/load_data.py
@@ -5,35 +5,33 @@ Script to load precisionFDA COVID 19
 """
 
 # libs
-from glob import glob
+import sqlite3
 import pandas as pd
 import numpy as np
-import csv_to_sqlite
 
 
 ### dir management
 train_data_dir = "../data/csv/train/"
 test_data_dir = "../data/csv/test/"
-
 # sqlite
 sqlite_dir = "../data/sqlite/"
 
-### list files 
-# train files
-train_files = glob(train_data_dir+"*.csv")
-# test files
-test_files = glob(train_data_dir+"*.csv")
-
-### convert csv files to sqlite 
-options = csv_to_sqlite.CsvOptions(typing_style="full", encoding="windows-1250") 
-csv_to_sqlite.write_csv(train_files, sqlite_dir+"covid_train.sqlite", options)
-csv_to_sqlite.write_csv(test_files, sqlite_dir+"covid_test.sqlite", options)
 
 ### load train data
-## as a loop
-# train_df = {}
-# for f in train_files[0:2]:
-#     train_df[f.split("\\")[1].split(".csv")[0]] = pd.read_csv(f)
-    
-#train_conditions = pd.read_csv(train_data_dir + "conditions.csv")
+# db
+train_db = sqlite3.connect(sqlite_dir+"covid_train.sqlite")
+
+# tables 
+conditions = pd.read_sql_query("SELECT * FROM conditions;", train_db)
+medications = pd.read_sql_query("SELECT * FROM medications;", train_db)
+
+pd.read_sql
+
+# qrys
+pd.read_sql_query("SELECT CODE, DESCRIPTION, COUNT(*) AS num_dx FROM conditions GROUP BY CODE, DESCRIPTION ORDER BY num_dx DESC;", train_db)
+
+# covid patients
+covid_pids = conditions[conditions.CODE == 840539006].PATIENT.unique()
+
+
 
---
+++ code/scratch.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 14 15:55:45 2020
+
+@author: roddy
+"""
+
+## as a loop
+# train_df = {}
+# for f in train_files[0:2]:
+#     train_df[f.split("\\")[1].split(".csv")[0]] = pd.read_csv(f)
+    
+#train_conditions = pd.read_csv(train_data_dir + "conditions.csv")
+
