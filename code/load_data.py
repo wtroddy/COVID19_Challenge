188c2da5be599cf29217caea8e8d5f5201d37f6b
Will Roddy <wtroddy@udel.edu>
2020-06-17 00:08:49 -0400

    additional work on xgboost model

--- code/load_data.py
+++ code/load_data.py
@@ -16,8 +16,13 @@ sqlite_dir = "../data/sqlite/"
 ### load train data
 # set db object
 train_db = sqlite3.connect(sqlite_dir+"covid_train.sqlite")
+test_db = sqlite3.connect(sqlite_dir+"covid_test.sqlite")
+
 # load training patient data
 train_pts = pd.read_sql_query("SELECT * FROM covid_patient_data", train_db)
+
+# load test data
+test_pts = pd.read_sql_query("SELECT * FROM covid_patient_data", test_db)
 
 ### example model
 # predictor variables
@@ -28,14 +33,17 @@ x.loc[:,'VENT_FLAG'] = x.loc[:,'VENT_FLAG'].astype('int')
 
 # outcome variable 
 y = train_pts['DECEASED']
+y = y.astype('bool')
 
 ### xgboost training 
 from xgboost import XGBClassifier 
-model = XGBClassifier()
+model = XGBClassifier(objective='binary:logistic')
 model.fit(x,y)
 
 ### xgboost testing
-y_pred = model.predict(x)
+y_pred = model.predict_proba(x)
+
+
 predictions = [round(value) for value in y_pred]
 # evaluate predictions
 from sklearn.metrics import accuracy_score
